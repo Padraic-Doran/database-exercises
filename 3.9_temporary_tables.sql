3a8ed522f31575837f75f2a5a26279b726e8009d
Padraic Doran <padraic.doran@gmail.com>
2019-09-08 17:43:08 -0500

    Still Working

--- 3.9_temporary_tables.sql
+++ 3.9_temporary_tables.sql
@@ -111,3 +111,121 @@ Group By dept_no) as table_3 On Table_1.dept_no = table_2.dept_no;
 
 
 
+use employees;
+
+/* select * from salaries; */
+
+/* select AVG(salary), STDDEV_POP(salary)
+from salaries as Salary_Info
+where to_date > NOW(); */
+
+/* SELECT DE.emp_no as EMPLOYEE_NO, DE.dept_no as DEPARTMENT_NO, D.dept_name as DEPT_NAME
+from employees as E
+Join dept_emp as DE
+on E.emp_no = DE.emp_no
+Join departments as D
+On DE.dept_no = D.dept_no
+WHERE DE.to_date > NOW()
+Order By DEPT_name; */
+
+
+Select 
+from departments
+where to_date > NOW()
+and department_no = 'd001';
+
+
+
+use bayes_812;
+
+CREATE TABLE employees_with_departments AS
+	SELECT 
+		emp_no
+		,first_name
+		,last_name
+		,dept_no
+		,dept_name
+	FROM 
+		employees.employees
+	JOIN 
+		employees.dept_emp 
+		USING(emp_no)
+	JOIN 
+		employees.departments 
+		USING(dept_no)
+	;
+	SELECT * FROM employees_with_departments
+	;
+	
+	
+	ALTER TABLE 
+		employees_with_departments 
+	ADD 
+		full_name VARCHAR(31)
+	;
+
+UPDATE 
+		employees_with_departments
+	SET
+		full_name = CONCAT(first_name,' ',last_name)
+	;
+	ALTER TABLE 
+		employees_with_departments 
+	DROP COLUMN
+		first_name
+	;
+	ALTER TABLE 
+		employees_with_departments 
+	DROP COLUMN
+		last_name
+	;
+	CREATE TEMPORARY TABLE Sal LIKE employees.salaries
+	;
+	INSERT INTO Sal SELECT * FROM employees.salaries
+	;
+	ALTER TABLE Sal ADD Zscore FLOAT(53)
+	;
+SELECT * FROM Sal
+	;
+	CREATE TEMPORARY TABLE SalInfo
+	SELECT 
+		AVG(Salary) SalAvg
+		,STDDEV(Salary) SalStdDev
+	FROM
+		Sal
+	WHERE
+		to_date > now()
+	;
+	SELECT * FROM SalInfo
+	;
+	UPDATE Sal s JOIN SalInfo si
+	SET
+		s.Zscore = ((s.Salary - si.SalAvg)/(si.SalStdDev))
+	;
+	SELECT 
+		ed.dept_name, 
+		((AVG(s.salary) - AVG(si.SalAvg))/(AVG(si.SalStdDev))) salary_z_score
+	FROM
+		employees_with_departments ed
+	JOIN
+		Sal s
+		USING(emp_no)
+	JOIN
+		SalInfo si
+	GROUP BY
+		ed.dept_name
+	;
+/* select * from employees_with_departments */
+-- select * from Sal;
+select * from SalInfo;
+
+
+use employees;
+
+Select d.dept_no, d.dept_name, SUM(s.salary), AVG(s.salary) 
+from departments as d
+join dept_emp as de
+on d.dept_no = de.dept_no
+join salaries as s 
+on de.emp_no = s.emp_no
+Group By d.`dept_name`
