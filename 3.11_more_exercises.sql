d4a7c8b5667c5b590c63d4381450e2b3b5c1942c
Padraic Doran <padraic.doran@gmail.com>
2019-09-09 14:58:26 -0500

    3.11 Exercises

--- 3.11_more_exercises.sql
+++ 3.11_more_exercises.sql
@@ -717,6 +717,49 @@ LIMIT 1001 OFFSET 999;
 SELECT * from `customer`
 LIMIT 100 OFFSET 100;
 
+/*
+ORDER BY statement
+Select all columns from the film table and order rows by the length field in ascending order.
+	*/
+	USE sakila;
+	SELECT * FROM film
+	ORDER BY length 
+	;
+	/*
+	
+	/*
+Select all distinct ratings from the film table ordered by rating in descending order.
+	*/
+	USE sakila;
+	SELECT DISTINCT rating
+	FROM film
+	ORDER BY rating DESC
+	;
+	
+	/*
+Select the payment date and amount columns from the payment table for the first 20 payments ordered by payment amount in descending order.
+	*/
+	USE sakila;
+	SELECT payment_date ,amount
+	FROM payment
+	ORDER BY amount DESC
+	LIMIT 20
+	;
+	
+	/*
+Select the title, description, special features, length, and rental duration columns from the film table for the first 10 films with behind the scenes footage under 2 hours in length and a rental duration between 5 and 7 days, ordered by length in descending order.
+	*/
+	USE sakila;
+	SELECT
+		title, description, special_features,length, rental_duration
+	FROM film
+	WHERE length < 120
+		AND rental_duration BETWEEN 5 AND 7
+		AND special_features LIKE '%Behind the Scenes%' 
+	ORDER BY length DESC
+	LIMIT 10
+	;
+
 /* JOINs
 
 Select customer first_name/last_name and actor first_name/last_name columns from performing a left join between the customer and actor column on the last_name column in each table. (i.e. customer.last_name = actor.last_name)
